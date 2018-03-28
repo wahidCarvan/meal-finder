c027adab3611ba0e657ebd2ee30f544c795e87aa
wahidc7101 <wahidc7101@gmail.com>
2018-03-28 19:58:08 -0400

    all the js files are working now

--- food.js
+++ food.js
@@ -12,16 +12,26 @@ const EDAMAM_API_KEY =
 const EDAMAM_APPLICATION_ID = '8b5edbbf'
 //
 function jsonpCallback(json) {
-  console.log(json);
+
 	 if(json.hits.length > 0) {
 		renderResult(json);
-    // $('.main-content').show()
-	 } else {
-	 	$('.fail-screen').show();
-    $('.main-content').hide();
+   
+	 } 
+   else {
+    showFailScreen();
+	 
 	 }
 }
 
+function showFailScreen() {
+    $('.fail-screen').show();
+    $('.main-content').hide();
+    $('.fail-screen').on('click', '.restart', function (event) {
+      //location.reload()
+      console.log(location);
+    location.reload();
+  });
+  };
 // $( "p" ).text( "<b>Some</b> new text." );
 
 
@@ -32,9 +42,9 @@ function jsonpCallback(json) {
 // getDataFromFoodApi takes 2 arguments search term and the callback 
 //a callback is a function is executed after another one is called.
 function placesCallback(results, status){
-console.log(results, status);
-
-
+   if(status != 'OK') {
+     return showFailScreen();
+   }  
       const markers = results.map(function(result){
 
       	return {
@@ -79,47 +89,21 @@ console.log(results, status);
       // }
 }
 function getDataFromBothApi(searchTerm) {
-	// let queryString =`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=${searchTerm}&key=AIzaSyCPpIUMEgvm4Hy3mQdkhAYONjNw-Kw3y7M`
-	// console.log(queryString);
-	//  $.ajax({
-	//  url: queryString,
- //  	dataType:'jsonp',
- //  	success: function(json, status, error){
- //  		console.log(json, status, error);
- //  	}
- // javascript user location google map
- // change pyrmont find out the out the users location
-  	   // let pyrmont = {lat: -33.867, lng: 151.195};
   	   navigator.geolocation.getCurrentPosition(function(position){
+        console.log('gotten current position', new Date())
+
   	   	console.log(position); 
   	   	  let service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
+          service.nearbySearch({
           location: {lat: position.coords.latitude, lng: position.coords.longitude},
           keyword:  searchTerm,
           radius: 500,
-       
           type: ['restaurant']
         }, placesCallback);
   	   		}, function (error){
   	   			 console.log(error);
   	   			});
 
-			
-
-  	 
-
-  // const query = {
-  // 	q:searchTerm,
-  // 	app_id: `${EDAMAM_APPLICATION_ID}`,
-  //   app_key: `${EDAMAM_API_KEY}`,
-  //   ingr: 10,
-  // }
-
-  //$ sign is jquery unless its a template literal. template literal is `${edamam_application_id}`
-  // console.log(query);
-  // $.getJSON(EDAMAM_SEARCH_URL, query, callback);
-  // we added the key value pairs manually using jsonp
-  // jsonp requests data and gets a callback
   $.ajax({
 
   	url: 'https://api.edamam.com/search?callback=jsonpCallback&ingr=10&app_id=8b5edbbf&app_key=8a8331400d84d570a5aa12258b227c04&q='+searchTerm,
@@ -189,17 +173,17 @@ $('.js-search-form').on('submit', function(event){
 	getDataFromBothApi($('.js-query-search').val());
 	//clears the search box
 	$('.js-query-search').val("");
-	});
-}
-// reload button NOT WORKING
-function reloadPage () {
-  $('main').on('click', '.restart', function (event) {
+  //calling reloadPage
+  $('.main-content').on('click', '.search-again', function (event) {
     location.reload();
-   
+
   });
+  $('.js-search-results').show();
+	});
 }
 $(watchSubmit);
 
+ 
 
 
 
