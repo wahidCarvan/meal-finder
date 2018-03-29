9ed08c8863733b0f6bfb1e9d077cb1179fdbd605
wahidc7101 <wahidc7101@gmail.com>
2018-03-29 12:21:39 -0400

    fixed the map

--- food.js
+++ food.js
@@ -10,17 +10,41 @@ const EDAMAM_SEARCH_URL = 'https://api.edamam.com/search'
 const EDAMAM_API_KEY =
 '8a8331400d84d570a5aa12258b227c04'	
 const EDAMAM_APPLICATION_ID = '8b5edbbf'
-//
+let finalSearchTerm = '';
 function jsonpCallback(json) {
 
   if(json.hits.length > 0) {
-    renderResult(json);
+   navigator.geolocation.getCurrentPosition(function(position){
+    console.log('gotten current position', new Date())
 
-  } 
-  else {
-    showFailScreen();
+    console.log(position); 
+     let map = new google.maps.Map(document.getElementById('map'), {
+      // lat and long updated to get users position
+          center: {lat: position.coords.latitude, lng: position.coords.longitude},
+          zoom: 15,
+          zoomControl: true,
+          zoomControlOptions: {
+              position: google.maps.ControlPosition.LEFT_CENTER
+          },
+      });
+        window.map = map;
+    let service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: {lat: position.coords.latitude, lng: position.coords.longitude},
+      keyword:  finalSearchTerm,
+      radius: 500,
+      type: ['restaurant']
+    }, placesCallback);
+  }, function (error){
+    console.log(error);
+  });
+   renderResult(json);
 
-  }
+ } 
+ else {
+  showFailScreen();
+
+}
 }
 
 function showFailScreen() {
@@ -58,31 +82,18 @@ function placesCallback(results, status){
    new google.maps.Marker(marker)
  })
  console.log(markers);
-    }
-    
-    function getDataFromBothApi(searchTerm) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        console.log('gotten current position', new Date())
+}
 
-        console.log(position); 
-        let service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: {lat: position.coords.latitude, lng: position.coords.longitude},
-          keyword:  searchTerm,
-          radius: 500,
-          type: ['restaurant']
-        }, placesCallback);
-      }, function (error){
-        console.log(error);
-      });
+function getDataFromBothApi(searchTerm) {
+ finalSearchTerm = searchTerm;
 
-      $.ajax({
+ $.ajax({
 
-       url: 'https://api.edamam.com/search?callback=jsonpCallback&ingr=10&app_id=8b5edbbf&app_key=8a8331400d84d570a5aa12258b227c04&q='+searchTerm,
-       dataType:'jsonp'
-     })
+   url: 'https://api.edamam.com/search?callback=jsonpCallback&ingr=10&app_id=8b5edbbf&app_key=8a8331400d84d570a5aa12258b227c04&q='+searchTerm,
+   dataType:'jsonp'
+ })
 
-    }
+}
 // make a function to render the results
 function renderResult(result) {
 	//hides the start page after the results are rendered
--- placeMap.js
+++ placeMap.js
@@ -1,16 +1,22 @@
+  
+
+
   // initilazies the map
-      function initMap() {
+  function initMap() {
         // displays the map on the page makes the map
         let map = new google.maps.Map(document.getElementById('map'), {
+            // intial location map is set to
           center: {lat: 39.833851, lng: -74.871826},
-          zoom: 18,
+          zoom: 16,
           zoomControl: true,
-         	zoomControlOptions: {
-        		position: google.maps.ControlPosition.LEFT_CENTER
-    		},
-        });
-    window.map = map;
+          zoomControlOptions: {
+              position: google.maps.ControlPosition.LEFT_CENTER
+          },
+      });
+        window.map = map;
 
-      };
+    };
+
+
 
 
