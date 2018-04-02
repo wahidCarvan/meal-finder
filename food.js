a7206e447b6497a17f121d26640e3402088cb74f
wahidc7101 <wahidc7101@gmail.com>
2018-04-02 17:52:27 -0400

    js is completed

--- food.js
+++ food.js
@@ -17,30 +17,7 @@ let finalSearchTerm = '';
 function jsonpCallback(json) {
 
   if(json.hits.length > 0) {
-   navigator.geolocation.getCurrentPosition(function(position){
-    console.log('gotten current position', new Date())
 
-    console.log(position); 
-     let map = new google.maps.Map(document.getElementById('map'), {
-      // lat and long updated to get users position
-          center: {lat: position.coords.latitude, lng: position.coords.longitude},
-          zoom: 15,
-          zoomControl: true,
-          zoomControlOptions: {
-              position: google.maps.ControlPosition.LEFT_CENTER
-          },
-      });
-        window.map = map;
-    let service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: {lat: position.coords.latitude, lng: position.coords.longitude},
-      keyword:  finalSearchTerm,
-      radius: 500,
-      type: ['restaurant']
-    }, placesCallback);
-  }, function (error){
-    console.log(error);
-  });
    renderResult(json);
 
  } 
@@ -73,29 +50,70 @@ function placesCallback(results, status){
  if(status != 'OK') {
    return showFailScreen();
  }  
- const markers = results.map(function(result){
-
-   return {
-    map: window.map,
-    title: result.name,
-    position: result.geometry.location
-  }
-
+//ternary operator
+//3 parts boolean
+//first expression
+//second expression
+// add the css inline for this part
+const markers = results.map(function(result){
+console.log(result);
+ let infowindow = new google.maps.InfoWindow({
+  content: `<div>
+<h1>${result.name}</h1>
+<p>${result.vicinity}</p>
+<p>${result.opening_hours.open_now?'open now':'closed'}</p>
+<p>Rating:${result.rating}</p>
+  </div>`
 });
- markers.forEach(function(marker){
-   new google.maps.Marker(marker)
- })
- console.log(markers);
+ const marker = new google.maps.Marker ({
+  map: window.map,
+  title: result.name,
+  position: result.geometry.location,
+})
+ marker.addListener('click', function() {
+  infowindow.open(map, marker);
+})
+return marker;
+});
+// markers.forEach(function(marker){
+//  new google.maps.Marker(marker)
+// })
+console.log(markers);
 }
 
 function getDataFromBothApi(searchTerm) {
  finalSearchTerm = searchTerm;
+// makes the call to edamam
+$.ajax({
+ url: 'https://api.edamam.com/search?callback=jsonpCallback&ingr=10&app_id=8b5edbbf&app_key=8a8331400d84d570a5aa12258b227c04&q='+searchTerm,
+ dataType:'jsonp'
+})
 
- $.ajax({
+// makes the call google places
+navigator.geolocation.getCurrentPosition(function(position){
+    // console.log('gotten current position', new Date())
 
-   url: 'https://api.edamam.com/search?callback=jsonpCallback&ingr=10&app_id=8b5edbbf&app_key=8a8331400d84d570a5aa12258b227c04&q='+searchTerm,
-   dataType:'jsonp'
- })
+    console.log(position); 
+    let map = new google.maps.Map(document.getElementById('map'), {
+      // lat and long updated to get users position
+      center: {lat: position.coords.latitude, lng: position.coords.longitude},
+      zoom: 15,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_CENTER
+      },
+    });
+    window.map = map;
+    let service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: {lat: position.coords.latitude, lng: position.coords.longitude},
+      keyword:  finalSearchTerm,
+      radius: 500,
+      type: ['restaurant']
+    }, placesCallback);
+  }, function (error){
+    console.log(error);
+  });
 
 }
 
@@ -120,13 +138,13 @@ function renderResult(result) {
   let listItem =
   `<div class ="js-food-item"> 
   <!--IMAGE -->
-  <img src="${result.hits[i].recipe.image}"/>
+  <div style="background-image: url(${result.hits[i].recipe.image}); background-size:cover; height: 200px; width: 100%;"> </div>
   <!--ingr-->
   <h1>${result.hits[i].recipe.label}</h1>
   <!--.join("") removes the commas since we are working with an array -->
   <ul>${recipes.join("")}</ul>
   <!--SOURCE-->
-  <a href="${result.hits[i].recipe.url}" target="_blank">recipe</a>
+  <a href="${result.hits[i].recipe.url}" target="_blank">Learn more about the recipe</a>
   </div>`
 
 	// grab the listItems and push in to the listItem
